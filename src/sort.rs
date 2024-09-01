dc14d9ae2cb9fb10909e87d7db35f8e9eeaf4bbc
Juan Castillo <juan@redpanda.com>
2024-09-01 11:22:23 -0500

    Sort the intermediate files

--- src/sort.rs
+++ src/sort.rs
@@ -27,9 +27,11 @@ async fn split(cfg: &Config) -> io::Result<Vec<File>> {
     for i in 0..no_intermediate_files {
         let b = b.clone();
         let name = cfg.file.to_owned().clone();
+        let int_file_dir = cfg.int_file_dir.clone();
         let int_file_size = cfg.int_file_size;
         set.spawn_blocking(move || {
-            let mut f = File::create(format!("int-{}.txt", i.to_string()))?;
+            fs::create_dir_all(&int_file_dir)?;
+            let mut f = File::create(format!("{}/{}.txt", int_file_dir, i.to_string()))?;
             let mut file = File::open(name.clone())?;
 
             let o = i as u64 * int_file_size;
@@ -41,9 +43,17 @@ async fn split(cfg: &Config) -> io::Result<Vec<File>> {
             // TODO: What if bytes_read < int_file_size? E.g. if the source file doesn't align with
             // int_file_size.
             let bytes_read = file.read(buf.as_mut())?;
+            let blocks_per_file = buf.len() / crate::BLOCK_SIZE;
+            let mut blocks = Vec::with_capacity(blocks_per_file);
+            for i in 0..blocks_per_file {
+                // TODO: is buf.take(crate::BLOCK_SIZE) better?
+                let offset = i * crate::BLOCK_SIZE;
+                blocks.push(&buf[offset..offset + crate::BLOCK_SIZE]);
+            }
+            blocks.sort_unstable();
 
             // TODO: check written bytes match the expected val.
-            match f.write(&buf[..bytes_read]) {
+            match f.write(&blocks.concat()) {
                 Ok(_) => {
                     b.put();
                     Ok(f)
